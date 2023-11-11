f24fb34aab32deafc57b42e7941f19d11016a0ab
Parivesh Srivastava <pariveshsrivastava1093@gmail.com>
2023-11-12 03:12:01 +0530

    basic rps

--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,134 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import '@/styles/globals.css'
+import { useState } from 'react';
 
 export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    //1 = rock
+    //2 = paper
+    //3 = scisors
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    let [start, setStart] = useState(true);
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+    let [resultPage, setResultPage] = useState(false);
+    let [animationPage, setAnimationPage] = useState(false);
+    let [result, setResult] = useState();
+    let [imga, setImga] = useState();
+    let [imgb, setImgb] = useState();
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+    let [scorePlayer, setScorePlayer] = useState(0);
+    let [scoreAi, setScoreAi] = useState(0);
+    // let resultPage = false;
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
+    let [aniText, setAniText] = useState("");
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    const image_list = ["https://img.icons8.com/ios/100/hand-rock.png", "https://img.icons8.com/ios/100/hand.png", "https://img.icons8.com/ios/100/hand-scissors--v1.png"];
+
+    function random(min, max) { // min and max included 
+        return Math.floor(Math.random() * (max - min + 1) + min)
+    }
+
+    function showText(message, speed) {
+        let i = 0;
+        let interval = setInterval(function () {
+            setAniText((prevAniText) => prevAniText + message[i]);
+            i++;
+            if (i >= message.length) {
+                clearInterval(interval);
+                setAnimationPage(false);
+            }
+        }, speed);
+    }
+
+    function record(a) {
+        let rand = random(1, 3);
+        console.log(a, rand);
+
+        setImga(image_list[a - 1]);
+        setImgb(image_list[rand - 1]);
+
+        if (a === rand) {
+            setResult("DRAW");
+        } else if ((a === 2 && rand === 1) || (a === 3 && rand === 2) || (a === 1 && rand === 3)) {
+            setScorePlayer(scorePlayer + 1);
+            setResult("YOU WIN");
+        } else {
+            setScoreAi(scoreAi + 1)
+            setResult("YOU LOSE");
+        }
+
+        setAniText(new String);
+        setResultPage(true);
+        setAnimationPage(true);
+
+        showText(" Choosing...", 130, () => {
+            setAnimationPage(false);
+        });
+        setTimeout(() => {
+            setResultPage(false);
+        }, 3000);
+
+        // setAnimationPage(false);
+    }
+
+    return (
+        <>
+            {
+                start ?
+                    <>
+                        <div className="flex justify-center pt-5">
+                            <p className="text-8xl sm:text-6xl font-light">ROCK PAPER SCISSORS</p>
+
+                        </div>
+                        <div className="flex justify-center pb-5" >
+                            <p className="font-extralight">- Parivesh</p>
+                        </div>
+                        <div className="flex justify-center"><button onClick={() => { setStart(false) }}>
+                            <p className="underline font-light">Start</p>
+                        </button>
+                        </div>
+                    </>
+                    : <>
+                        {resultPage ?
+                            <>
+                                {
+                                    animationPage ?
+                                        <>
+                                            <p className="flex justify-center mt-10 mb-5 text-3xl font-light" id="msg">
+                                                {aniText}
+                                            </p>
+                                        </>
+                                        : <>
+                                            <div className="">
+                                                {/* <p>Results page</p> */}
+                                                <p className="flex justify-center mt-10 mb-5 text-3xl font-light">{result}</p>
+                                                <div className="flex justify-center align-middle ">
+                                                    <img className="m-7" width="100" height="100" src={imga} alt="hand-rock" />
+                                                    <img className="m-7" width="100" height="100" src={imgb} alt="hand-scissors--v1" />
+                                                </div>
+                                            </div>
+                                        </>
+                                }
+
+                            </>
+                            :
+                            <>
+                                <div className="">
+                                    <p className="flex justify-center mt-10 mb-5 text-4xl font-light">ROCK PAPER SCISSORS</p>
+                                    <p className="flex justify-center m-5">AI COUNT : {scoreAi}&nbsp; |&nbsp; Player Count : {scorePlayer}</p>
+                                    <div className="flex justify-center align-middle ">
+                                        <button onClick={() => record(1)}>
+                                            <img className="m-7" width="100" height="100" src="https://img.icons8.com/ios/100/hand-rock.png" alt="hand-rock" />
+                                        </button>
+                                        <button onClick={() => record(2)}>
+                                            <img className="m-7" width="100" height="100" src="https://img.icons8.com/ios/100/hand.png" alt="hand" />
+                                        </button>
+                                        <button onClick={() => record(3)}>
+                                            <img className="m-7" width="100" height="100" src="https://img.icons8.com/ios/100/hand-scissors--v1.png" alt="hand-scissors--v1" />
+                                        </button>
+                                    </div>
+                                </div>
+                            </>}
+                    </>
+            }
+        </>
+    )
 }
